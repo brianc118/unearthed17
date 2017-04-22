59dea751a92ce1f69f2df7eae53f4a94a6107251
mitchmcdee <mitchmcdee@gmail.com>
2017-04-23 00:45:45 +1000

    Added main data processing script

--- mitch/main.py
+++ mitch/main.py
@@ -2,11 +2,50 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+from copy import deepcopy
 import re
 
 Base = declarative_base()
-engine = create_engine('mysql+pymysql://root@localhost/ivms', echo=False)
+engine = create_engine('mysql+pymysql://root@localhost/ivms', echo=True)
 Session = sessionmaker(bind=engine)
+
+class Trip(Base):
+    __tablename__ = 'Trips'
+
+    TripUID = Column(Integer, primary_key=True)
+    Date = Column(String(512))  #11/1/2016
+    AssetName = Column(String(512)) 
+    AssetID = Column(Integer)
+    AssetExtra = Column(String(512))
+    AssetName2 = Column(String(512))
+    AssetID2 = Column(Integer)
+    AssetExtra2 = Column(String(512))
+    SiteName = Column(String(512))
+    OriginalDriverName = Column(String(512))
+    TrailerInfo = Column(String(512))
+    TripID = Column(Integer)
+    ActivityStartDateTime = Column(String(512))  #HH:MM:SS
+    DepartureTime = Column(String(512))
+    ArrivalTime = Column(String(512))
+    ActivityEndDateTime = Column(String(512))  #11/1/2016 HH:MM:SS
+    TimeZone = Column(String(512))
+    DrivingTimePercentage = Column(String(512))
+    StandingTimePercentage = Column(String(512))
+    IdleTimePercentage = Column(String(512))
+    TotalDrivingTime = Column(String(512))
+    TotalStandingTime = Column(String(512))
+    TotalDuration = Column(String(512)) # ???
+    TotalIdleTime = Column(String(512))
+    ParkingTime = Column(String(512))
+    StartOdometer = Column(Float)
+    EndOdometer = Column(Float)
+    TotalDistanceTravelled = Column(Float) #
+    TotalFuelUsed = Column(Float)
+    FuelConsumptionMeasured = Column(Float)
+    TotalCO2Emission = Column(Float)
+    TotalEngineSeconds = Column(String(512))
+    AverageSpeed = Column(Float) #
+    MaxSpeed = Column(Float) #
 
 class Event(Base):
     __abstract__ = True
@@ -123,6 +162,37 @@ emptyInfo = {'cornering_1': [],
              'seat_belt_passenger': []
             }
 
+tripInfo = {'start': None,
+            'end': None,
+            'TotalDuration': None,
+            'TripID': 0,
+            'TotalDistanceTravelled': 0,
+            'AverageSpeed': 0,
+            'MaxSpeed': 0,
+            'cornering_1': [],
+            'cornering_2': [],
+            'cornering_3': [],
+            'geofence_speeding_1': [],
+            'geofence_speeding_2': [],
+            'geofence_speeding_3': [],
+            'public_speeding_1': [],
+            'public_speeding_2': [],
+            'public_speeding_3': [],
+            'journey_management': [],
+            'harsh_acceleration': [],
+            'harsh_breaking': [],
+            'excessive_breaking': [],
+            'night_driving': [],
+            'no_go_zone': [],
+            'seat_belt_driver': [],
+            'seat_belt_passenger': []
+           };
+
+
+####################################################################################
+#       GET ALL DAT DRIVER DATA
+####################################################################################
+
 session = Session()
 drivers = {}
 
@@ -132,7 +202,7 @@ for i in session.query(Alert).order_by('Drivers_ID'):
 
     driver = int(i.Drivers_ID)
     if driver not in drivers:
-        drivers[driver] = emptyInfo
+        drivers[driver] = deepcopy(emptyInfo)
 
     dt = datetime.strptime(i.EventStartTime + ' ' + i.EventStartDate, '%H:%M:%S %d/%m/%Y')
 
@@ -147,14 +217,14 @@ for i in session.query(Corner).order_by('EventCornerId'):
 
     driver = int(i.AssetID)
     if driver not in drivers:
-        drivers[driver] = emptyInfo
+        drivers[driver] = deepcopy(emptyInfo)
 
     dt = datetime.strptime(i.EventStartTime + ' ' + i.EventStartDate, '%H:%M:%S %d/%m/%Y')
 
     event = i.Cat
     for eventPattern, eventType in events:
-       if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
-           break
+        if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
+            break
 
 for i in session.query(Speed).order_by('EventSpeedId'):
     if not datetime.strptime(i.EventStartDate, '%d/%m/%Y').month == 11:
@@ -162,14 +232,14 @@ for i in session.query(Speed).order_by('EventSpeedId'):
 
     driver = int(i.Drivers_ID)
     if driver not in drivers:
-        drivers[driver] = emptyInfo
+        drivers[driver] = deepcopy(emptyInfo)
 
     dt = datetime.strptime(i.EventStartTime + ' ' + i.EventStartDate, '%H:%M:%S %d/%m/%Y')
 
     event = i.EventDescription
     for eventPattern, eventType in events:
-       if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
-           break
+        if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
+            break
 
 for i in session.query(Year).order_by('EventYearId'):
     if not datetime.strptime(i.EventStartDate, '%d/%m/%Y').month == 11:
@@ -177,14 +247,14 @@ for i in session.query(Year).order_by('EventYearId'):
 
     driver = int(i.Drivers_ID)
     if driver not in drivers:
-        drivers[driver] = emptyInfo
+        drivers[driver] = deepcopy(emptyInfo)
 
     dt = datetime.strptime(i.EventStartTime + ' ' + i.EventStartDate, '%H:%M:%S %d/%m/%Y')
 
     event = i.EventDescription
     for eventPattern, eventType in events:
-       if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
-           break
+        if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
+            break
 
 for i in session.query(Other).order_by('EventOtherId'):
     if not datetime.strptime(i.EventStartDate, '%d/%m/%Y').month == 11:
@@ -192,11 +262,75 @@ for i in session.query(Other).order_by('EventOtherId'):
 
     driver = int(i.Drivers_ID)
     if driver not in drivers:
-        drivers[driver] = emptyInfo
+        drivers[driver] = deepcopy(emptyInfo)
 
     dt = datetime.strptime(i.EventStartTime + ' ' + i.EventStartDate, '%H:%M:%S %d/%m/%Y')
 
     event = i.EventDescription
     for eventPattern, eventType in events:
-       if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
-           break
+        if add_event(drivers[driver], eventType, re.search(eventPattern, event), dt):
+            break
+
+
+####################################################################################
+#       OKAY ITS TRIP TIME
+####################################################################################
+
+
+driverTrips = {}
+tripCount = 0
+for i in session.query(Trip).yield_per(8192):
+    if not datetime.strptime(i.Date, '%m/%d/%Y').month == 11:
+        continue
+
+    driver = i.AssetID
+    if driver not in driverTrips:
+        driverTrips[driver] = []
+
+    start = datetime.strptime(i.Date + ' ' + i.ActivityStartDateTime, '%m/%d/%Y %H:%M:%S')
+    end = datetime.strptime(i.ActivityEndDateTime, '%m/%d/%Y %H:%M')
+    
+    newTrip = deepcopy(tripInfo)
+    newTrip['start'] = start
+    newTrip['end'] = end
+    newTrip['TripID'] = i.TripID;
+    newTrip['TotalDuration'] = start - end
+    newTrip['TotalDistanceTravelled'] = i.TotalDistanceTravelled
+    newTrip['AverageSpeed'] = i.AverageSpeed
+    newTrip['MaxSpeed'] = i.MaxSpeed
+
+    driverTrips[driver].append(newTrip)
+    tripCount += 1
+
+print('Total trips = {}'.format(tripCount))
+print('Total trip ids = {}'.format(len(driverTrips.keys())))
+print('Total driver ids = {}'.format(len(drivers.keys())))
+print('Intersect = {}'.format(len(set(list(driverTrips.keys())).intersection(list(drivers.keys())))))
+
+count = 0
+for driver_id in drivers.keys():
+    driver = drivers[driver_id];
+    for k in emptyInfo.keys():
+        if k not in driver:
+            continue
+
+        for time in driver[k]:
+            if driver_id not in driverTrips:
+                continue
+
+            for trip in driverTrips[driver_id]:
+                if trip['start'] <= time and time <= trip['end']:
+                    trip[k].append(time)
+                    count += 1
+                    break
+
+print(len(list(driverTrips.keys())), count)
+
+# print("DUMPING TRIP")
+
+# import pickle
+
+# with open('trip.pickle', 'wb') as handle:
+#     pickle.dump(trip, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+print("DONE")
